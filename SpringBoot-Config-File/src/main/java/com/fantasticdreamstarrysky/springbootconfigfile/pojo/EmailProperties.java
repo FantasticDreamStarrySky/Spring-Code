7222df79cce807488a9f49363fc0f05364fd063e
🌃梦幻◎星空🌃 <Fantastic.Dream.Starry.Sky@outlook.com>
2024-02-23 07:34:05 +0800

    使用 @ConfigurationProperties 注解来自动获取配置文件里的值

--- SpringBoot-Config-File/src/main/java/com/fantasticdreamstarrysky/springbootconfigfile/pojo/EmailProperties.java
+++ SpringBoot-Config-File/src/main/java/com/fantasticdreamstarrysky/springbootconfigfile/pojo/EmailProperties.java
@@ -1,6 +1,5 @@
 package com.fantasticdreamstarrysky.springbootconfigfile.pojo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -15,25 +14,21 @@ public class EmailProperties {
     /**
      * 发件人邮箱
      */
-    @Value("${email.user}")
     public String user;
 
     /**
      * 发件人邮箱授权码
      */
-    @Value("${email.code}")
     public String code;
 
     /**
      * 发件人邮箱对应的服务器域名,如果是163邮箱:smtp.163.com   qq邮箱: smtp.qq.com
      */
-    @Value("${email.host}")
     public String host;
 
     /**
      * 身份验证开关
      */
-    @Value("${email.auth}")
     private boolean auth;
 
     public String getHost() {
