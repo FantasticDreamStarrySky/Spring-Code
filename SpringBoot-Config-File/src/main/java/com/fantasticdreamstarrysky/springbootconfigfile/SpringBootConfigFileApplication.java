6c96b6bb551fa9d908b56879571e2c582dac98b0
🌃梦幻◎星空🌃 <Fantastic.Dream.Starry.Sky@outlook.com>
2024-02-23 06:22:27 +0800

    创建 SpringBoot-Config-File 项目

---
+++ SpringBoot-Config-File/src/main/java/com/fantasticdreamstarrysky/springbootconfigfile/SpringBootConfigFileApplication.java
@@ -0,0 +1,13 @@
+package com.fantasticdreamstarrysky.springbootconfigfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigFileApplication.class, args);
+    }
+
+}
