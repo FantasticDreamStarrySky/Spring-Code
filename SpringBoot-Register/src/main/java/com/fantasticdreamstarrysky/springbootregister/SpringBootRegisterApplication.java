5d350e786924d213ff1d418eaee5d9152b442128
🌃梦幻◎星空🌃 <Fantastic.Dream.Starry.Sky@outlook.com>
2024-02-25 06:37:14 +0800

    注入Country对象

--- SpringBoot-Register/src/main/java/com/fantasticdreamstarrysky/springbootregister/SpringBootRegisterApplication.java
+++ SpringBoot-Register/src/main/java/com/fantasticdreamstarrysky/springbootregister/SpringBootRegisterApplication.java
@@ -1,13 +1,30 @@
 package com.fantasticdreamstarrysky.springbootregister;
 
+import cn.itcast.pojo.Country;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * @author 🌃梦幻◎星空🌃
+ */
 @SpringBootApplication
 public class SpringBootRegisterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRegisterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringBootRegisterApplication.class, args);
+
+        Country country = context.getBean(Country.class);
+        System.out.println(country);
+    }
+
+    /**
+     * 注入Country对象
+     */
+    @Bean
+    public Country country() {
+        return new Country();
+    }
 
 }
