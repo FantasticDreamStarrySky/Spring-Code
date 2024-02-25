bbfbea68a35435ccfcfebb7a54714e6f96fb52c3
🌃梦幻◎星空🌃 <Fantastic.Dream.Starry.Sky@outlook.com>
2024-02-25 21:07:02 +0800

    :tada: 创建 SpringBoot-Auto-Config 项目

---
+++ SpringBoot-Auto-Config/src/main/java/com/fantasticdreamstarrysky/springbootautoconfig/SpringBootAutoConfigApplication.java
@@ -0,0 +1,13 @@
+package com.fantasticdreamstarrysky.springbootautoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoConfigApplication.class, args);
+    }
+
+}
---
+++ SpringBoot-Auto-Config/src/test/java/com/fantasticdreamstarrysky/springbootautoconfig/SpringBootAutoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasticdreamstarrysky.springbootautoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutoConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
