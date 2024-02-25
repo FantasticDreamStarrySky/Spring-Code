8db42f085a6817077585377d7f0a277ab944646f
🌃梦幻◎星空🌃 <Fantastic.Dream.Starry.Sky@outlook.com>
2024-02-25 21:40:17 +0800

    Bean自动配置

--- SpringBoot-Auto-Config/src/main/java/com/fantasticdreamstarrysky/springbootautoconfig/SpringBootAutoConfigApplication.java
+++ SpringBoot-Auto-Config/src/main/java/com/fantasticdreamstarrysky/springbootautoconfig/SpringBootAutoConfigApplication.java
@@ -2,12 +2,16 @@ package com.fantasticdreamstarrysky.springbootautoconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+/**
+ * @author 🌃梦幻◎星空🌃
+ */
 @SpringBootApplication
 public class SpringBootAutoConfigApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAutoConfigApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringBootAutoConfigApplication.class, args);
+        System.out.println(context.getBean("dispatcherServlet"));
+        System.out.println(context.getBean("province"));
     }
-
 }
